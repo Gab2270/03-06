3a660b29acb047bbbe33f8d991972660a4835e4f
Gab2270 <142789170+Gab2270@users.noreply.github.com>
2024-06-03 11:24:15 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo } from '@expo/vector-icons';
+import Home from "./components/Home";
+import Sobre from "./components/Sobre";
 
+
+const Tabs = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs.Navigator
+        screenOptions={{
+          tabBarStyle:{
+            backgroundColor: '#eab990',
+          },
+
+          tabBarLabelStyle: {
+            fontSize: 18,
+            fontWeight: 'bold',
+          },
+
+          tabBarActiveBackgroundColor: '#eab990',
+
+          tabBarInactiveTintColor: '#f00',
+
+          tabBarActiveTintColor: '#150b13',
+
+          tabBarLabelPosition: 'beside-icon',
+        }}
+        >
+        <Tabs.Screen 
+        name="Home" 
+        component={Home} 
+        options={
+          {
+            tabBarIcon: ({ color, size }) => (
+              <Entypo name="Perfil" color={ color } size={ size } />
+            )
+          }
+        }
+        />
+        <Tabs.Screen 
+        name="Sobre" 
+        component={Sobre}
+        options={
+          {
+            tabBarIcon: ({ color, size }) => (
+              <Entypo name="circle-with-plus" color={ color } size={ size } />
+            )
+          }
+        }
+        />
+      </Tabs.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
